3f885a68cb660c03255169184842d6ba343ccee4
Kostya <rayjackson@live.ru>
2025-09-17 07:04:44 +0300

    Run server only in dev mode

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'tsdown'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: 'esm',
   outDir: 'dist',
   clean: true,
   dts: true,
-  onSuccess: 'node dist/index.js',
+  onSuccess: !isProd ? 'node dist/index.js' : undefined,
 })
